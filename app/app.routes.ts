ef566e15a993149b913e149767aa20b964c466c3
Juparave <juparave@deuxbits.com>
2016-07-19 15:45:21 -0500

    Routing

---
+++ app/app.routes.ts
@@ -0,0 +1,23 @@
+import { provideRouter, RouterConfig} from '@angular/router';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+
+const routes: RouterConfig = [
+  {
+    path: '', redirectTo: 'dashboard', pathMatch: 'full' 
+  },
+  {
+    path: 'heroes', component: HeroesComponent 
+  },
+  {
+    path: 'detail/:id', component: HeroDetailComponent 
+  },
+  {
+    path: 'dashboard', component: DashboardComponent 
+  }
+];
+
+export const appRouterProviders = [
+  provideRouter(routes)
+];
