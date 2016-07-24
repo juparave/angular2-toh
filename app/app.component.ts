6de2697df4cf40155e5b4e1d15ed489c7ba8adc8
Juparave <juparave@deuxbits.com>
2016-07-24 11:04:07 -0500

    Http, part 2: observables (hero-search)

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { HeroService } from './hero.service';
 
+import './rxjs-extensions';
+
+
 @Component({
   selector: 'my-app',
   directives: [ROUTER_DIRECTIVES],
